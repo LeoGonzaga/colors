9795082eefcf5076e63ed5db03e9e24c0b5212ab
LeoGonzaga <sacredlotus05@gmail.com>
2021-04-29 21:17:45 -0300

    adicionando funcao de geração de cores

--- src/screen/home/Home.tsx
+++ src/screen/home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   ActionButton,
   Colors,
@@ -10,28 +10,50 @@ import {
   Wrapper,
 } from "./styled";
 
-const HomeScreen = (): JSX.Element => (
-  <Container>
-    <LeftColumn>
-      <Wrapper>
-        <Title color="#AD12B0" animation>
-          Colors
-        </Title>
-        <Title color="#fff">and</Title>
-        <Title color="#7B7B74">Criative</Title>
-      </Wrapper>
-      <ActionButton>Gerar paleta</ActionButton>
-    </LeftColumn>
-    <RightColumn>
-      <ColorsContainer>
-        <Colors color="#EDA751" />
-        <Colors color="#F1642E" />
-        <Colors color="#DF5243" />
-        <Colors color="#C9384A" />
-        <Colors color="#6C121D" />
-      </ColorsContainer>
-    </RightColumn>
-  </Container>
-);
+const HomeScreen = (): JSX.Element => {
+  const [colors, setColors] = useState([
+    "#EDA751",
+    "#F1642E",
+    "#DF5243",
+    "#C9384A",
+    "#6C121D",
+  ]);
+
+  const generetedColors = useCallback(() => {
+    let newColors = [];
+    for (let index = 0; index < 5; index++) {
+      let color = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+      console.log(color);
+      newColors.push(color);
+    }
+
+    setColors(newColors);
+  }, []);
+
+  useEffect(() => {
+    generetedColors();
+  }, []);
+  return (
+    <Container>
+      <LeftColumn>
+        <Wrapper>
+          <Title color="#AD12B0" animation>
+            Colors
+          </Title>
+          <Title color="#fff">and</Title>
+          <Title color="#7B7B74">Criative</Title>
+        </Wrapper>
+        <ActionButton onClick={generetedColors}>Gerar paleta</ActionButton>
+      </LeftColumn>
+      <RightColumn>
+        <ColorsContainer>
+          {colors.map((color, index) => (
+            <Colors key={index} color={color} />
+          ))}
+        </ColorsContainer>
+      </RightColumn>
+    </Container>
+  );
+};
 
 export default HomeScreen;
